617066b529bbe838737215afc5131d57dc36bb08
neverovskii <neverovskii.na@gmail.com>
2022-11-09 03:09:15 +0300

    small improvements

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 static mut FIBONACCI_COUNTER: u128 = 0;
+static DEFAULT_NTH: u8 = 30;
 
 fn main() {
     let mut stdin: Stdin = io::stdin();
@@ -14,37 +15,32 @@ fn main() {
 
     stdout.flush().unwrap();
 
-let fibonacci_nth: u8 = match stdin.lock().lines().next() {
-    None => {
-        println!("\nFibonacci Number is not provided, defaulting to 45");
-        45
-    }
-    Some(result) => match result {
-        Ok(string) => match u8::from_str_radix(&string, 10) {
-            Ok(number) => match number {
-                1..=60 => number,
-                _ => {
-                    println!("Provided number outside of 1-60 range, defaulting to 45");
-                    45
+    let fibonacci_nth: u8 = match stdin.lock().lines().next() {
+        None => {
+            println!("Fibonacci Number is not provided, defaulting to {}", DEFAULT_NTH);
+            DEFAULT_NTH
+        }
+        Some(result) => match result {
+            Ok(string) => match u8::from_str_radix(&string, 10) {
+                Ok(number) => match number {
+                    1..=60 => number,
+                    _ => {
+                        println!("Provided number is outside of 1-60 range, defaulting to {}", DEFAULT_NTH);
+                        DEFAULT_NTH
+                    }
+                },
+                Err(error) => {
+                    println!("Error processing number from the given input: ({}), defaulting to {}", error, DEFAULT_NTH);
+                    DEFAULT_NTH
                 }
             },
             Err(error) => {
-                println!(
-                    "\nError processing number from the giving input: ({}), defaulting to 45",
-                    error
-                );
-                45
+                println!("Something has gone terribly wrong: ({}), defaulting to {}", error, DEFAULT_NTH);
+                DEFAULT_NTH
             }
         },
-        Err(error) => {
-            println!(
-                "\nSomething has gone terribly wrong: ({}), defaulting to 45",
-                error
-            );
-            45
-        }
-    },
-};
+    };
+
     let ordinal_suffix: &str = get_ordinal_suffix(fibonacci_nth);
     let before_iter_time: Instant = Instant::now();
     let fibonacci_nth_value_iter: u128 = get_fibonacci_iterative(fibonacci_nth);
@@ -56,6 +52,7 @@ let fibonacci_nth: u8 = match stdin.lock().lines().next() {
         fibonacci_nth_value_iter,
         after_iter_time.as_secs_f32()
     );
+
     unsafe {
         thread::spawn(move || loop {
             let counter_thread: u128 = FIBONACCI_COUNTER;
@@ -71,6 +68,8 @@ let fibonacci_nth: u8 = match stdin.lock().lines().next() {
                 }
             }
         });
+        thread::sleep(Duration::from_millis(5));
+
         let before_rec_time: Instant = Instant::now();
         let fibonacci_nth_value_rec: u128 = get_fibonacci_recursive(fibonacci_nth);
         let after_rec_time: Duration = before_rec_time.elapsed();
@@ -81,10 +80,15 @@ let fibonacci_nth: u8 = match stdin.lock().lines().next() {
             fibonacci_nth_value_rec,
             after_rec_time.as_secs_f32()
         );
-        println!(
-            "The recursive implementation was {} times slower, than iterative!",
-            after_rec_time.as_secs_f32() / after_iter_time.as_secs_f32()
-        );
+
+        let diff_time: f32 = after_rec_time.as_secs_f32() / after_iter_time.as_secs_f32();
+        if diff_time > 1.0 {
+            println!(
+                "The recursive implementation was {:.2} times slower, than iterative!",
+                diff_time
+            );
+        }
+
         stdout.write(b"Press Enter to exit...").unwrap();
         stdout.flush().unwrap();
         let _ = stdin.read(&mut [0u8]).unwrap();
@@ -124,7 +128,7 @@ fn get_fibonacci_recursive(num_iter: u8) -> u128 {
 fn get_ordinal_suffix(int: u8) -> &'static str {
     let suffix: &str = match int {
         4..=20 => "th",
-        _ => match get_last_digit(int) {
+        _ => match int % 10 {
             1 => "st",
             2 => "nd",
             3 => "rd",
@@ -134,22 +138,10 @@ fn get_ordinal_suffix(int: u8) -> &'static str {
     return suffix;
 }
 
-fn get_last_digit(int: u8) -> u8 {
-    let mut v_copy = int;
-    let mut buf: Vec<u8> = Vec::with_capacity(3);
-
-    while v_copy > 0 {
-        let n = v_copy % 10;
-        v_copy = v_copy / 10;
-        buf.push(n);
-    }
-
-    buf.last().copied().unwrap_or(0)
-}
-
 fn progress_bar(current: u128, maximum: u128, segments: usize, info: &str) -> () {
-    let full_segments: usize =
-        ((current as f64 / maximum as f64) * segments as f64).floor() as usize;
+    let percentage: f64 = current as f64 / maximum as f64;
+
+    let full_segments: usize = (percentage * segments as f64).floor() as usize;
 
     let bar: String = str::repeat("#", full_segments) + &str::repeat("-", segments - full_segments);
 
@@ -167,7 +159,7 @@ fn progress_bar(current: u128, maximum: u128, segments: usize, info: &str) -> ()
         }
     };
 
-    print!("\r[{}] {} / {} {} {}", bar, current, maximum, state, info);
+    print!("\r[{}] {:.2}% {} / {} {} {}", bar, percentage * 100 as f64, current, maximum, state, info);
 
     if current == maximum {
         println!()
